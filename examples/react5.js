9bc0611a535b4bd52fcdbd7986f7c8e749447acf
yildiztugba <okra.yildiz@gmail.com>
2022-02-25 11:31:49 +0100

    conditional style of react

---
+++ examples/react5.js
@@ -0,0 +1,14 @@
+class ConditionalStyle extends React.Component {
+	render() {
+  	return (
+    	<div style={{ color: Math.random() < 0.5 ? 'green': 'red' }}>
+    	  How do you like this?
+    	</div>
+    );
+  }	
+}
+
+ReactDOM.render(
+	<ConditionalStyle />,
+  mountNode,
+);
